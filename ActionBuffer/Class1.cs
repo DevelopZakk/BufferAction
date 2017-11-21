57cd4a51250beb7beeed4782ea4d32e65085440d
developzakk <developzakk@outlook.com>
2017-11-21 21:04:44 +0800

    BufferAction

---
+++ ActionBuffer/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionBuffer
+{
+
+    public interface IAction
+    {
+        bool DoAction();
+    }
+
+    public class BufferAction
+    {
+        private readonly int _timers;
+        private readonly int _counter;
+        private readonly IAction _action;
+
+        private int _curentTimer;
+        private int _currentCounter;
+
+        public bool ActionStart;
+        public bool ActionDone;
+
+        public BufferAction(int timers, int counter, IAction action)
+        {
+            _timers = timers;
+            _counter = counter;
+            _action = action;
+        }
+
+        public void RecieveResult(bool result)
+        {
+            if (ActionStart)
+            {
+                return;
+            }
+
+            if (result)
+            {
+                _curentTimer = 0;
+                return;
+            }
+
+            _curentTimer += 1;
+            if (_curentTimer >= _timers)
+            {
+                _currentCounter += 1;
+                _curentTimer = 0;
+            }
+
+            if (_currentCounter >= _counter)
+            {
+                ActionStart = true;
+                ActionDone= _action.DoAction();
+            }
+
+        }
+    }
+}
---
+++ ActionBufferTests/BufferActionTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ActionBuffer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionBuffer.Tests
+{
+    [TestClass()]
+    public class BufferActionTests
+    {
+        [TestMethod()]
+        public void RecieveResultTest()
+        {
+            var act = new Act();
+            var buffer = new BufferAction(1,1, act);
+
+            buffer.RecieveResult(true);
+            Assert.AreEqual(false,buffer.ActionStart);
+            Assert.AreEqual(0,act.Counter);
+
+            buffer.RecieveResult(true);
+            Assert.AreEqual(false, buffer.ActionStart);
+            Assert.AreEqual(0, act.Counter);
+
+            buffer.RecieveResult(false);
+            Assert.AreEqual(true,buffer.ActionStart);
+            Assert.AreEqual(1,act.Counter);
+
+            buffer.RecieveResult(false);
+            Assert.AreEqual(1,act.Counter);
+
+
+            var act2= new Act();
+            var buf2 = new BufferAction(3,2,act2);
+
+            var test = new TestProvider();
+            foreach (var testSet in test.TestSet1())
+            {
+                buf2.RecieveResult(testSet.Test);
+                Assert.AreEqual(act2.Counter,testSet.Counter);
+                Assert.AreEqual(testSet.ActionStart,buf2.ActionStart);
+            }
+        }
+    }
+
+    public class TestProvider
+    {
+        public List<TestSet> TestSet1()
+        {
+            return new List<TestSet>()
+            {
+                new TestSet(false,0,false),
+                new TestSet(false,0,false),
+                new TestSet(true,0,false),
+                new TestSet(true,0,false),
+                new TestSet(false,0,false),
+                new TestSet(false,0,false),
+                new TestSet(false,0,false),
+                new TestSet(true,0,false),
+                new TestSet(false,0,false),
+                new TestSet(true,0,false),
+                new TestSet(false,0,false),
+                new TestSet(false,0,false),
+                new TestSet(false,1,true),
+                new TestSet(false,1,true),
+                new TestSet(false,1,true),
+            };
+        }
+    }
+
+    public class TestSet
+    {
+        public bool Test { get; }
+        public bool ActionStart { get; }
+        public int Counter { get; }
+
+        public TestSet(bool test,int Counter, bool actionStart)
+        {
+            Test = test;
+            ActionStart = actionStart;
+            Counter = counter;
+        }
+    }
+
+    public class Act : IAction
+    {
+        public int Counter { get; set; }
+
+        public bool DoAction()
+        {
+            Counter += 1;
+            return true;
+        }
+    }
+}
